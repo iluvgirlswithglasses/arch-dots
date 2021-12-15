3c655dd52529eef299f5f28ebbf6de2831a97801
iluvgirlswithglasses <luunamdat204@gmail.com>
2021-12-15 13:10:23 +0700

    laptop's dots is now seperated

---
+++ awesome-lap/autostart.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+export EXCSYS=/home/mika/excsys
+export PATH=$PATH:$EXCSYS
+
+function run {
+  if ! pgrep $1 ;
+  then
+    $@&
+  fi
+}
+
+### screen settings
+# run xrandr --output VGA-1 --primary --mode 1360x768 --pos 0x0 --rotate normal
+# run xrandr --output HDMI2 --mode 1920x1080 --pos 1920x0 --rotate normal --output HDMI1 --primary --mode 1920x1080 --pos 0x0 --rotate normal --output VIRTUAL1 --off
+# run xrandr --output LVDS1 --mode 1366x768 --rotate normal --output VGA1 --primary --mode 1920x1080 --rotate normal --right-of LVDS1
+# autorandr horizontal
+
+### run services
+run nm-applet
+# run caffeine
+# run pamac-tray
+run variety
+# run xfce4-power-manager
+# run blueberry-tray
+run picom
+run /usr/lib/polkit-gnome/polkit-gnome-authentication-agent-1
+run numlockx on
+# run volumeicon
+run nitrogen --restore
+run dunst
---
+++ awesome-lap/widgets/calendar.lua
@@ -0,0 +1,171 @@
+local gears                                       = require("gears")
+local colors                                      = require("utils.colors")
+local awful                                       = require("awful")
+local wibox                                       = require("wibox")
+local dpi                                         = require("beautiful.xresources").apply_dpi
+
+function calendar(widget)
+      local popup = awful.popup {
+          widget = {
+            {
+              {
+                text = "── Calendar ──",
+                align="center",
+                widget = wibox.widget.textbox
+              },
+              widget = wibox.container.background
+            },
+            {
+              {
+                {
+                  text = "",
+                  align = "center",
+                  font = "JetBrainsMono Nerd Font 60",
+                  widget = wibox.widget.textbox
+                },
+                right = 20,
+                left = 30,
+                widget = wibox.container.margin
+              },
+              {
+                {
+                  {
+                    
+                    {
+                      {
+                        text = "Wish myself a productive day!",
+                        widget = wibox.widget.textbox
+                      },
+                      {
+                        {
+                          {
+                            text = "",
+                            widget = wibox.widget.textbox
+                          },
+                          fg = colors.red,
+                          widget = wibox.container.background,
+                        },
+                        {
+                          format = " %B", -- month
+                          refresh = 60,
+                          widget = wibox.widget.textclock
+                        },
+                        margins = 5,
+                        widget = wibox.container.margin,
+                        layout = wibox.layout.fixed.horizontal
+                      },
+                     {
+                        {
+                          {
+                            text = "",
+                            widget = wibox.widget.textbox
+                          },
+                          fg = colors.green,
+                          widget = wibox.container.background,
+                        },
+                        {
+                          format = " %d", -- day
+                          refresh = 60,
+                          widget = wibox.widget.textclock
+                        },
+                        margins = 5,
+                        widget = wibox.container.margin,
+                        layout = wibox.layout.fixed.horizontal
+                      },
+                     {
+                        {
+                          {
+                            text = "",
+                            widget = wibox.widget.textbox
+                          },
+                          fg = colors.pink,
+                          widget = wibox.container.background,
+                        },
+                        {
+                          format = " %Y", -- year
+                          refresh = 60,
+                          widget = wibox.widget.textclock
+                        },
+                        margins = 5,
+                        widget = wibox.container.margin,
+                        layout = wibox.layout.fixed.horizontal
+                      },
+                      {
+                        {
+                          {
+                            text = "",
+                            widget = wibox.widget.textbox
+                          },
+                          fg = colors.yellow,
+                          widget = wibox.container.background,
+                        },
+                        {
+                          format = " %A", -- weekday
+                          refresh = 60,
+                          widget = wibox.widget.textclock
+                        },
+                        margins = 5,
+                        widget = wibox.container.margin,
+                        layout = wibox.layout.fixed.horizontal
+                      },
+                      {
+                        {
+                          {
+                            text = "",
+                            widget = wibox.widget.textbox
+                          },
+                          fg = colors.frost.lightest,
+                          widget = wibox.container.background,
+                        },
+                        {
+                          format = " %H:%M%p ",
+                          refresh = 60,
+                          widget = wibox.widget.textclock
+                        },
+                        margins = 5,
+                        widget = wibox.container.margin,
+                        layout = wibox.layout.fixed.horizontal
+                      },                    
+                      expand = 'none',
+                      widget = wibox.container.background,
+                      layout = wibox.layout.fixed.vertical
+                    },
+
+                    margins = 10,
+                    widget = wibox.container.margin,
+                  },
+
+                  bg = colors.polar.darkest.."5a",
+                  shape = gears.shape.rounded_rect,
+                  widget = wibox.container.background
+                },
+                margins = 10,
+                widget = wibox.container.margin
+              },
+              layout = wibox.layout.fixed.horizontal,
+
+            },
+            layout = wibox.layout.fixed.vertical,
+            widget = wibox.container.margin
+          },
+          bg = colors.bg,
+          fg = "#ffffff",
+          ontop=true,
+          shape        = gears.shape.rounded_rect,
+          visible      = false,
+      }
+      widget:connect_signal("button::press",
+        function()
+          if popup.visible then
+            popup.visible = false
+          else
+            popup:move_next_to(mouse.current_widget_geometry)
+            popup.visible = true
+          end
+        end
+      )
+
+
+      return widget
+    end
+return calendar
