ec56052af4a658d66b4f8d084489ff47cefaf901
iluvgirlswithglasses <luunamdat204@gmail.com>
2023-11-02 10:40:06 +0700

    indenting

--- browser-exts/githubFlash/background.js
+++ browser-exts/githubFlash/background.js
@@ -9,20 +9,20 @@ Update the page action's title and icon to reflect its state.
 */
 function toggleCSS(tab) {
 
-  function gotTitle(title) {
-    if (title === TITLE_APPLY) {
-      browser.pageAction.setIcon({tabId: tab.id, path: "icons/on.svg"});
-      browser.pageAction.setTitle({tabId: tab.id, title: TITLE_REMOVE});
-      browser.tabs.insertCSS({code: CSS});
-    } else {
-      browser.pageAction.setIcon({tabId: tab.id, path: "icons/off.svg"});
-      browser.pageAction.setTitle({tabId: tab.id, title: TITLE_APPLY});
-      browser.tabs.removeCSS({code: CSS});
-    }
-  }
+	function gotTitle(title) {
+		if (title === TITLE_APPLY) {
+			browser.pageAction.setIcon({tabId: tab.id, path: "icons/on.svg"});
+			browser.pageAction.setTitle({tabId: tab.id, title: TITLE_REMOVE});
+			browser.tabs.insertCSS({code: CSS});
+		} else {
+			browser.pageAction.setIcon({tabId: tab.id, path: "icons/off.svg"});
+			browser.pageAction.setTitle({tabId: tab.id, title: TITLE_APPLY});
+			browser.tabs.removeCSS({code: CSS});
+		}
+	}
 
-  let gettingTitle = browser.pageAction.getTitle({tabId: tab.id});
-  gettingTitle.then(gotTitle);
+	let gettingTitle = browser.pageAction.getTitle({tabId: tab.id});
+	gettingTitle.then(gotTitle);
 }
 
 /*
@@ -30,11 +30,11 @@ Initialize the page action: set icon and title, then show.
 Only operates on tabs whose URL's protocol is applicable.
 */
 function initializePageAction(tab) {
-  if (tab.url.includes(APPLY_ON)) {
-    browser.pageAction.setIcon({tabId: tab.id, path: "icons/off.svg"});
-    browser.pageAction.setTitle({tabId: tab.id, title: TITLE_APPLY});
-    browser.pageAction.show(tab.id);
-  }
+	if (tab.url.includes(APPLY_ON)) {
+		browser.pageAction.setIcon({tabId: tab.id, path: "icons/off.svg"});
+		browser.pageAction.setTitle({tabId: tab.id, title: TITLE_APPLY});
+	browser.pageAction.show(tab.id);
+	}
 }
 
 /*
@@ -42,16 +42,16 @@ When first loaded, initialize the page action for all tabs.
 */
 let gettingAllTabs = browser.tabs.query({});
 gettingAllTabs.then((tabs) => {
-  for (let tab of tabs) {
-    initializePageAction(tab);
-  }
+	for (let tab of tabs) {
+		initializePageAction(tab);
+	}
 });
 
 /*
 Each time a tab is updated, reset the page action for that tab.
 */
 browser.tabs.onUpdated.addListener((id, changeInfo, tab) => {
-  initializePageAction(tab);
+	initializePageAction(tab);
 });
 
 /*
